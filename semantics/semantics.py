b6eef2512d64ef7ced6c7ace11d850c7496ab8eb
Qing Li <dylan.liqing@gmail.com>
2020-11-20 17:47:59 -0800

    update

--- semantics/semantics.py
+++ semantics/semantics.py
@@ -93,7 +93,7 @@ class Semantics(object):
 class DreamCoder(object):
     def __init__(self):
         args = commandlineArguments(
-            enumerationTimeout=10, activation='tanh', iterations=1, recognitionTimeout=3600,
+            enumerationTimeout=20, activation='tanh', iterations=3, recognitionTimeout=3600,
             a=3, maximumFrontier=10, topK=2, pseudoCounts=30.0,
             helmholtzRatio=0.5, structurePenalty=1.,
             CPUs=numberOfCPUs(),
