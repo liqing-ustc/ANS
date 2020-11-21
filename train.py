55b1c134b874afeff5d0f3d235e40a85a89fa6e2
Qing Li <dylan.liqing@gmail.com>
2020-11-20 22:39:32 -0800

    update

--- train.py
+++ train.py
@@ -110,7 +110,7 @@ def train(model, num_epochs=500, n_epochs_per_eval = 1):
     max_len = float("inf")
     curriculum_strategy = dict([
         (0, 1),
-        (10, 3),
+        (5, 3),
         (50, 5),
         (100, 1e9)
     ])
