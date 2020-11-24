d250650655a1c71ffe3acb0881a831b254d3475a
Qing Li <dylan.liqing@gmail.com>
2020-11-23 21:45:53 -0800

    update

--- unit_test.py
+++ unit_test.py
@@ -6,9 +6,11 @@ from dreamcoder.domains.hint.hintPrimitives import McCarthyPrimitives
 _ = McCarthyPrimitives()
 from dreamcoder.program import Program
 from semantics.semantics import ProgramWrapper
-# pg = Program.parse("(lambda (lambda (fix2 $1 $0 (lambda (lambda (lambda (if0 $0 $1 ($2 (incr $1) (decr0 $0)))))))))")
-pg = Program.parse("(lambda (lambda (fix2 $1 $0 (lambda (lambda (lambda (if0 $0 $1 ($2 $0 (decr0 $1)))))))))")
-# print(pg.runWithArguments([1,2]))
+pg = Program.parse("(lambda (lambda (fix2 $1 $0 (lambda (lambda (lambda (if0 $0 $1 ($2 (incr $1) (decr0 $0)))))))))")
 pg = ProgramWrapper(pg)
-print(pg(9, 0))
+print(pg(1, 2))
+# pg = Program.parse("(lambda (lambda (fix2 $1 $0 (lambda (lambda (lambda (if0 $0 $1 ($2 $0 (decr0 $1)))))))))")
+pg = Program.parse("(lambda (lambda (fix2 $1 $0 (lambda (lambda (lambda (if0 $0 $1 (incr ($2 $1 (decr0 $0))))))))))")
+pg = ProgramWrapper(pg)
+print(pg(1, 1))
 pass
