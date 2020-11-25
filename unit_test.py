97fb3c5cf146e0edee6a27e225230bd48d7662aa
Qing Li <dylan.liqing@gmail.com>
2020-11-25 10:04:36 -0800

    update

--- unit_test.py
+++ unit_test.py
@@ -13,4 +13,9 @@ print(pg(1, 2))
 pg = Program.parse("(lambda (lambda (fix2 $1 $0 (lambda (lambda (lambda (if0 $0 $1 (incr ($2 $1 (decr0 $0))))))))))")
 pg = ProgramWrapper(pg)
 print(pg(1, 1))
+
+# Multiply
+pg = Program.parse("(lambda (lambda (fix2 $1 $0 (lambda (lambda (lambda (#(lambda (lambda (fix2 $1 $0 (lambda (lambda (lambda (if0 $0 $1 ($2 (incr $1) (decr0 $0))))))))) (if0 $0 0 ($2 $0 (decr0 $1))) $0)))))))")
+pg = ProgramWrapper(pg)
+print(pg(0, 2))
 pass
