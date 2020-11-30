d954da82302a38c46d75f5abb0cbcec49267274e
Qing Li <dylan.liqing@gmail.com>
2020-11-30 15:25:10 -0800

    update

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 # python train.py --curriculum --semantics --output-dir=outputs/exp0/ >outputs/exp0/main.log
 # python train.py --semantics --output-dir=outputs/exp1/ >outputs/exp1/main.log
-OUTPUT=./outputs/exp2/ python train.py --curriculum --epochs=500 --output-dir=$OUTPUT >$OUTPUT/main.log 2>$OUTPUT/dreamcoder.log
+export OUTPUT=./outputs/exp2/ 
+python train.py --curriculum --epochs=500 --output-dir=$OUTPUT >$OUTPUT/main.log 2>$OUTPUT/dreamcoder.log
