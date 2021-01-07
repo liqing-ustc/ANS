939376448b70ae571e444ba3ef3cc18b2919bc30
Qing Li <dylan.liqing@gmail.com>
2021-01-07 10:28:50 -0800

    update

--- train.py
+++ train.py
@@ -202,6 +202,9 @@ def train(model, args, st_epoch=0):
         print('-' * 30)
         print('Epoch {}/{} (max_len={}, data={})'.format(epoch, args.epochs - 1, max_len, len(train_set)))
 
+        perception_acc, head_acc, result_acc = evaluate(model, train_dataloader)
+        print('{} (Perception Acc={:.2f}, Head Acc={:.2f}, Result Acc={:.2f})'.format('val', 100*perception_acc, 100*head_acc, 100*result_acc))
+
         for _ in range(len(model.learning_schedule)):
             with torch.no_grad():
                 model.train()
