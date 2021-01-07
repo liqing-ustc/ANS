cb88918daca89316028945eee85ff735da1d6999
Qing Li <dylan.liqing@gmail.com>
2021-01-07 10:33:40 -0800

    update

--- perception/perception.py
+++ perception/perception.py
@@ -34,7 +34,7 @@ class Perception(object):
     def train(self):
         # self.model.train()
         self.model.eval()
-        self.training = True
+        self.training = False
     
     def eval(self):
         self.model.eval()
