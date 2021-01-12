25726ac552b0067e053df8c2d504b85eb1162a9f
Qing Li <dylan.liqing@gmail.com>
2021-01-11 21:45:58 -0800

    update

--- run_0.sh
+++ run_0.sh
@@ -6,21 +6,21 @@ export CUDA_VISIBLE_DEVICES=0
 # mkdir -p $OUTPUT
 # python train.py --curriculum --output-dir=$OUTPUT --seed=$seed >$OUTPUT/main.log
 
-# Learn 1 meaning
-# Learn syntax
-export OUTPUT=./outputs/exp_perception_semantics_curriculum/seed_${seed}/
-mkdir -p $OUTPUT
-python train.py --perception --semantics --curriculum --output-dir=$OUTPUT --seed=$seed >$OUTPUT/main.log
+# # Learn 1 meaning
+# # Learn syntax
+# export OUTPUT=./outputs/exp_perception_semantics_curriculum/seed_${seed}/
+# mkdir -p $OUTPUT
+# python train.py --perception --semantics --curriculum --output-dir=$OUTPUT --seed=$seed >$OUTPUT/main.log
 
-# Learn perception
-export OUTPUT=./outputs/exp_syntax_semantics_curriculum/seed_${seed}/
-mkdir -p $OUTPUT
-python train.py --syntax --semantics --curriculum --output-dir=$OUTPUT --seed=$seed >$OUTPUT/main.log
+# # Learn perception
+# export OUTPUT=./outputs/exp_syntax_semantics_curriculum/seed_${seed}/
+# mkdir -p $OUTPUT
+# python train.py --syntax --semantics --curriculum --output-dir=$OUTPUT --seed=$seed >$OUTPUT/main.log
 
-# Learn semantics
-export OUTPUT=./outputs/exp_perception_syntax_curriculum/seed_${seed}/
-mkdir -p $OUTPUT
-python train.py --perception --syntax --curriculum --output-dir=$OUTPUT --seed=$seed >$OUTPUT/main.log
+# # Learn semantics
+# export OUTPUT=./outputs/exp_perception_syntax_curriculum/seed_${seed}/
+# mkdir -p $OUTPUT
+# python train.py --perception --syntax --curriculum --output-dir=$OUTPUT --seed=$seed >$OUTPUT/main.log
 
 
 # Learn 2 meanings
@@ -29,12 +29,12 @@ export OUTPUT=./outputs/exp_semantics_curriculum/seed_${seed}/
 mkdir -p $OUTPUT
 python train.py --semantics --curriculum --output-dir=$OUTPUT --seed=$seed >$OUTPUT/main.log
 
-# Learn syntax and semantics
-export OUTPUT=./outputs/exp_perception_curriculum/seed_${seed}/
-mkdir -p $OUTPUT
-python train.py --perception --curriculum --output-dir=$OUTPUT --seed=$seed >$OUTPUT/main.log
+# # Learn syntax and semantics
+# export OUTPUT=./outputs/exp_perception_curriculum/seed_${seed}/
+# mkdir -p $OUTPUT
+# python train.py --perception --curriculum --output-dir=$OUTPUT --seed=$seed >$OUTPUT/main.log
 
-# Learn perception and semantics
-export OUTPUT=./outputs/exp_syntax_curriculum/seed_${seed}/
-mkdir -p $OUTPUT
-python train.py --syntax --curriculum --output-dir=$OUTPUT --seed=$seed >$OUTPUT/main.log
+# # Learn perception and semantics
+# export OUTPUT=./outputs/exp_syntax_curriculum/seed_${seed}/
+# mkdir -p $OUTPUT
+# python train.py --syntax --curriculum --output-dir=$OUTPUT --seed=$seed >$OUTPUT/main.log
