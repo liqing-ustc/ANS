d92d3ef34d0a024b3fcc71b25bcb4974cb61d7c8
Qing Li <dylan.liqing@gmail.com>
2021-01-20 09:03:02 -0800

    Update

--- baselines/run.sh
+++ baselines/run.sh
@@ -1,18 +1,22 @@
-# export ENC_LAYERS=1
-# export DEC_LAYERS=1
-# python train.py --curriculum --enc_layers=$ENC_LAYERS --dec_layers=$DEC_LAYERS >outputs/enc_${ENC_LAYERS}_dec_${DEC_LAYERS}.log
+export ENC_LAYERS=1
+export DEC_LAYERS=1
+python train.py --curriculum --enc_layers=$ENC_LAYERS --dec_layers=$DEC_LAYERS >outputs/enc_${ENC_LAYERS}_dec_${DEC_LAYERS}.log
 
-# export ENC_LAYERS=3
-# export DEC_LAYERS=1
-# python train.py --curriculum --enc_layers=$ENC_LAYERS --dec_layers=$DEC_LAYERS >outputs/enc_${ENC_LAYERS}_dec_${DEC_LAYERS}.log
+export ENC_LAYERS=3
+export DEC_LAYERS=1
+python train.py --curriculum --enc_layers=$ENC_LAYERS --dec_layers=$DEC_LAYERS >outputs/enc_${ENC_LAYERS}_dec_${DEC_LAYERS}.log
 
-# export ENC_LAYERS=3
-# export DEC_LAYERS=3
-# python train.py --curriculum --enc_layers=$ENC_LAYERS --dec_layers=$DEC_LAYERS >outputs/enc_${ENC_LAYERS}_dec_${DEC_LAYERS}.log
+export ENC_LAYERS=3
+export DEC_LAYERS=3
+python train.py --curriculum --enc_layers=$ENC_LAYERS --dec_layers=$DEC_LAYERS >outputs/enc_${ENC_LAYERS}_dec_${DEC_LAYERS}.log
 
-# export ENC_LAYERS=6
-# export DEC_LAYERS=6
-# python train.py --curriculum --enc_layers=$ENC_LAYERS --dec_layers=$DEC_LAYERS >outputs/enc_${ENC_LAYERS}_dec_${DEC_LAYERS}.log
+export ENC_LAYERS=6
+export DEC_LAYERS=6
+python train.py --curriculum --enc_layers=$ENC_LAYERS --dec_layers=$DEC_LAYERS >outputs/enc_${ENC_LAYERS}_dec_${DEC_LAYERS}.log
+
+export ENC_LAYERS=3
+export DEC_LAYERS=1
+python train.py --curriculum --enc_layers=$ENC_LAYERS --dec_layers=$DEC_LAYERS >outputs/enc_${ENC_LAYERS}_dec_${DEC_LAYERS}.log
 
 export ENC_LAYERS=6
 export DEC_LAYERS=1
