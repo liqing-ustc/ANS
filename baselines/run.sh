05171d1ba12e6d5bd040e135a394e87eb61e8b83
Qing Li <dylan.liqing@gmail.com>
2021-01-22 10:29:31 -0800

    Update

--- baselines/run.sh
+++ baselines/run.sh
@@ -26,12 +26,16 @@
 # export DEC_LAYERS=1
 # python train.py --perception --enc_layers=$ENC_LAYERS --dec_layers=$DEC_LAYERS >outputs/symbol_enc_${ENC_LAYERS}_dec_${DEC_LAYERS}.log
 
-# Tranformer with symbol input
-export MODEL=TRAN ENC_LAYERS=1 DEC_LAYERS=1 HEAD=1
-python train.py --seq2seq=${MODEL} --perception --curriculum --enc_layers=$ENC_LAYERS --dec_layers=$DEC_LAYERS --nhead=${HEAD} --epochs=1000 --epochs_eval=100 --dropout=0.1 >outputs/${MODEL}_symbol_enc_${ENC_LAYERS}_dec_${DEC_LAYERS}_head_${HEAD}.log
+# # Tranformer with symbol input
+# export MODEL=TRAN ENC_LAYERS=1 DEC_LAYERS=1 HEAD=1
+# python train.py --seq2seq=${MODEL} --perception --curriculum --enc_layers=$ENC_LAYERS --dec_layers=$DEC_LAYERS --nhead=${HEAD} --epochs=1000 --epochs_eval=100 --dropout=0.1 >outputs/${MODEL}_symbol_enc_${ENC_LAYERS}_dec_${DEC_LAYERS}_head_${HEAD}.log
 
+# export MODEL=TRAN ENC_LAYERS=3 DEC_LAYERS=3 HEAD=4
+# python train.py --seq2seq=${MODEL} --perception --curriculum --enc_layers=$ENC_LAYERS --dec_layers=$DEC_LAYERS --nhead=${HEAD} --epochs=1000 --epochs_eval=100 --dropout=0.1 >outputs/${MODEL}_symbol_enc_${ENC_LAYERS}_dec_${DEC_LAYERS}_head_${HEAD}.log
+
+# export MODEL=TRAN ENC_LAYERS=6 DEC_LAYERS=6 HEAD=8
+# python train.py --seq2seq=${MODEL} --perception --curriculum --enc_layers=$ENC_LAYERS --dec_layers=$DEC_LAYERS --nhead=${HEAD} --epochs=1000 --epochs_eval=100 --dropout=0.1 >outputs/${MODEL}_symbol_enc_${ENC_LAYERS}_dec_${DEC_LAYERS}_head_${HEAD}.log
+
+# Tranformer with image input
 export MODEL=TRAN ENC_LAYERS=3 DEC_LAYERS=3 HEAD=4
-python train.py --seq2seq=${MODEL} --perception --curriculum --enc_layers=$ENC_LAYERS --dec_layers=$DEC_LAYERS --nhead=${HEAD} --epochs=1000 --epochs_eval=100 --dropout=0.1 >outputs/${MODEL}_symbol_enc_${ENC_LAYERS}_dec_${DEC_LAYERS}_head_${HEAD}.log
-
-export MODEL=TRAN ENC_LAYERS=6 DEC_LAYERS=6 HEAD=8
-python train.py --seq2seq=${MODEL} --perception --curriculum --enc_layers=$ENC_LAYERS --dec_layers=$DEC_LAYERS --nhead=${HEAD} --epochs=1000 --epochs_eval=100 --dropout=0.1 >outputs/${MODEL}_symbol_enc_${ENC_LAYERS}_dec_${DEC_LAYERS}_head_${HEAD}.log
+python train.py --seq2seq=${MODEL} --curriculum --enc_layers=$ENC_LAYERS --dec_layers=$DEC_LAYERS --nhead=${HEAD} --epochs=1000 --epochs_eval=100 --dropout=0.1 >outputs/${MODEL}_enc_${ENC_LAYERS}_dec_${DEC_LAYERS}_head_${HEAD}.log
